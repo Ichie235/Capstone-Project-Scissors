4bf1569e188af8a714bb0510e47de09971f17e98
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2023-07-06 12:50:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
           }
         />
 
-        <Route path="/register" element={<Signup />} />
+        <Route path="register" element={<Signup />} />
       </Routes>
     </div>
   );
