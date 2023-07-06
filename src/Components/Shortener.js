f096060e454adb1987b588757707d233d81cba9c
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2023-07-06 13:47:51 +0100

    Update Shortener.js

--- src/Components/Shortener.js
+++ src/Components/Shortener.js
@@ -20,7 +20,7 @@ function Shortener({ loginData, setLoginData, apiKey, setApiKey }) {
 
     let api_url =
       //   process.env.NODE_ENV === 'production'
-      `http://localhost:8000/api/urls/?user=${loginData.email}`;
+      `https://urlshortener-api-4qsn.onrender.com/api/urls/?user=${loginData.email}`;
     // : `http://localhost:5000/api/urls/?user=${loginData.email}`;
 
     console.log(
@@ -85,7 +85,7 @@ function Shortener({ loginData, setLoginData, apiKey, setApiKey }) {
       : `Bearer ${loginData.token}`;
 
     if (url.match(regex)) {
-      let api_url = `http://localhost:8000/api/urls/encrypt/`;
+      let api_url = `https://urlshortener-api-4qsn.onrender.com/api/urls/encrypt/`;
 
       let headers = {
         "Content-Type": "application/json",
@@ -131,7 +131,7 @@ function Shortener({ loginData, setLoginData, apiKey, setApiKey }) {
       : `Bearer ${loginData.token}`;
 
     if (url.match(regex)) {
-      let api_url = `http://localhost:8000/api/urls/`;
+      let api_url = `https://urlshortener-api-4qsn.onrender.com/api/urls/`;
 
       let headers = {
         "Content-Type": "application/json",
